9af28ca33f861679131de53f6858ce70e83d6b50
Davidson Souza <iokwsg9ex@relay.firefox.com>
2023-08-08 18:45:32 -0300

    Document api

--- src/api.rs
+++ src/api.rs
@@ -1,11 +1,18 @@
+//SPDX-License-Identifier: MIT
+
+//! This is a simple REST API that can be used to query Utreexo data. You can get the roots
+//! of the accumulator, get a proof for a leaf, and get a block and the associated UData.
 use std::str::FromStr;
 
+use crate::prover::{Requests, Responses};
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use futures::{channel::mpsc::Sender, lock::Mutex, SinkExt};
 use rustreexo::accumulator::node_hash::NodeHash;
-use crate::prover::{Requests, Responses};
 
+/// This is the state of the actix-web server that will be passed as reference by each
+/// callback function. It contains a sender that can be used to send requests to the prover.
 struct AppState {
+    /// Sender to send requests to the prover.
     sender: Mutex<
         Sender<(
             Requests,
@@ -14,6 +21,8 @@ struct AppState {
     >,
 }
 
+/// This function is used to send a request to the prover and wait for the response, and
+/// return the response or an error.
 async fn perform_request(
     data: &web::Data<AppState>,
     request: Requests,
@@ -28,6 +37,8 @@ async fn perform_request(
     receiver.await.unwrap()
 }
 
+/// The handler for the `/proof/{hash}` endpoint. It returns a proof for the given hash, if
+/// it exists.
 async fn get_proof(hash: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
     let hash = hash.into_inner();
     let hash = NodeHash::from_str(&hash);
@@ -42,6 +53,7 @@ async fn get_proof(hash: web::Path<String>, data: web::Data<AppState>) -> impl R
         Err(e) => HttpResponse::InternalServerError().body(e),
     }
 }
+/// The handler for the `/block/{height}` endpoint. It returns the block at the given height.
 async fn get_block_by_height(height: web::Path<u32>, data: web::Data<AppState>) -> impl Responder {
     let height = height.into_inner();
     let res = perform_request(&data, Requests::GetBlockByHeight(height)).await;
@@ -51,6 +63,8 @@ async fn get_block_by_height(height: web::Path<u32>, data: web::Data<AppState>) 
         Err(e) => HttpResponse::NotAcceptable().body(e),
     }
 }
+
+/// The handler for the `/roots` endpoint. It returns the roots of the accumulator.
 async fn get_roots(data: web::Data<AppState>) -> HttpResponse {
     let res = perform_request(&data, Requests::GetRoots).await;
     match res {
@@ -59,6 +73,8 @@ async fn get_roots(data: web::Data<AppState>) -> HttpResponse {
         Err(e) => HttpResponse::NotAcceptable().body(e),
     }
 }
+
+/// This function creates the actix-web server and returns a future that can be awaited.
 pub async fn create_api(
     request: Sender<(
         Requests,
