6240a09ccd67bc8b9b18aa262eef75cda5bbc237
Michael Zaikin <michael.zaikin@starkware.co>
2024-12-17 21:08:43 +0000

    Use optimal poseidon hash impl for two elements

--- src/udata.rs
+++ src/udata.rs
@@ -512,7 +512,7 @@ pub mod shinigami_udata {
         // **both** children are not empty.
         fn parent_hash(left: &Self, right: &Self) -> Self {
             if let (PoseidonHash::Hash(left), PoseidonHash::Hash(right)) = (left, right) {
-                return PoseidonHash::Hash(poseidon_hash(left, right));
+                return PoseidonHash::Hash(poseidon_hash(*left, *right));
             }
 
             // This should never happen, since rustreexo won't call this method unless both children
